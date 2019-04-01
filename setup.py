4b1f6639c22b3abd00d30d43755de9c2734c461e
ravenoak <ravenoak@virtualxistenz.com>
2019-04-01 00:32:19 -0700

    Updates setup.py, moving some metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ PROJECT_NAME = 'Flask-arango-orm'
 PROJECT_RELEASE = '0.1.0'
 PROJECT_VERSION = '.'.join(PROJECT_RELEASE.split('.')[:2])
 INSTALL_REQUIRES = [
-    'arango-orm',
-    'Flask', ]
+    'arango-orm>=0.5.3',
+    'Flask>=1.0.0', ]
 SETUP_REQUIRES = [
     'setuptools',
     'pytest-runner',
@@ -21,38 +21,15 @@ TESTS_REQUIRES = [
 
 
 def readme():
-    with open('README.rst') as f:
-        return f.read()
+    with open('README.rst') as fh:
+        return fh.read()
 
 
 setup(name=PROJECT_NAME,
       version=PROJECT_RELEASE,
       description='Flask extension for arango-orm',
       license='LGPL-3.0',
-      license_file='LICENSE.txt',
       long_description=readme(),
-      classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
-          'Environment :: Plugins',
-          'Environment :: Web Environment',
-          'Framework :: Flask',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: GNU Lesser General Public License v3 '
-          'or later (LGPLv3+)',
-          'Programming Language :: Python :: 3.6',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3 :: Only',
-          'Topic :: Database',
-          'Topic :: Internet :: WWW/HTTP :: WSGI',
-          'Topic :: Software Development :: Libraries :: Python Modules', ],
-      author="Caitlyn O'Hanna",
-      author_email='ravenoak@virtualxistenz.com',
-      url='https://github.com/QuaererePlatform/Flask-arango-orm',
-      project_urls={
-          'Documentation': 'http://flask-arango-orm.readthedocs.io/',
-          'Source Code': 'https://github.com/QuaererePlatform/Flask-arango'
-                         '-orm',
-      },
       packages=find_packages(exclude=['docs', 'tests']),
       zip_safe=True,
       test_suite='tests',
