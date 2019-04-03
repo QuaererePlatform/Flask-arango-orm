1bd05f528b20763cd10342266a39306bb747bd04
ravenoak <ravenoak@virtualxistenz.com>
2019-04-02 17:17:02 -0700

    Adds circleci improvements

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@
 """Setup for Flask-arango-orm
 """
 
+import os
+import sys
+
 from setuptools import find_packages, setup
+from setuptools.command.install import install
 
 PROJECT_NAME = 'Flask-arango-orm'
 PROJECT_RELEASE = '0.1.0'
@@ -25,6 +29,35 @@ def readme():
         return fh.read()
 
 
+# Taken from https://circleci.com/blog/continuously-deploying-python-\
+# packages-to-pypi-with-circleci/
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != PROJECT_RELEASE:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, PROJECT_RELEASE
+            )
+            sys.exit(info)
+
+
+class WriteRequirementsCommand(install):
+    """Writes all package requirements into requirements.txt"""
+    description = 'creates requirements.txt'
+
+    def run(self):
+        header = '# Generated file, do not edit\n'
+        all_requirements = INSTALL_REQUIRES + SETUP_REQUIRES + TESTS_REQUIRES
+        all_requirements = [I + '\n' for I in all_requirements]
+        all_requirements.insert(0, header)
+        with open('requirements.txt', 'w') as fh:
+            fh.writelines(all_requirements)
+
+
 setup(name=PROJECT_NAME,
       version=PROJECT_RELEASE,
       description='Flask extension for arango-orm',
@@ -45,4 +78,7 @@ setup(name=PROJECT_NAME,
               'project': ('setup.py', PROJECT_NAME),
               'version': ('setup.py', PROJECT_VERSION),
               'release': ('setup.py', PROJECT_RELEASE),
-              'source_dir': ('setup.py', 'docs')}})
+              'source_dir': ('setup.py', 'docs'), }, },
+      cmdclass={
+          'mk_reqs': WriteRequirementsCommand,
+          'verify': VerifyVersionCommand, }, )
